4f9c916446bf5a986d240931d08e94e1696918de
Dhanushka Premasiri <dhanushka.p@calcey.com>
2023-10-06 17:47:36 +0530

    Add products entity

---
+++ API/Data/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Data
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int Quantity { get; set; }
+    }
+}
