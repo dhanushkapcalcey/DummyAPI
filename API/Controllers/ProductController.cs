27c5d09e6ddb0fb3a34522b72efffe89b26cc8b4
Dhanushka Premasiri <dhanushka.p@calcey.com>
2023-10-08 15:44:01 +0530

    Added new two endpoints

--- API/Controllers/ProductController.cs
+++ API/Controllers/ProductController.cs
@@ -1,4 +1,7 @@
 ﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,11 +11,40 @@ namespace API.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private readonly DataContext _dataContext;
+        private readonly IProductRepository _productRepository;
 
-        public ProductController(DataContext dataContext) 
+        public ProductController(IProductRepository productRepository) 
         {
-            _dataContext = dataContext;
+            _productRepository = productRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProductDto>> AddProduct(ProductDto productDto)
+        {
+            if(!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState);
+            }
+
+            var product = new Product
+            {
+                Name = productDto.Name,
+                Quantity = productDto.Quantity,
+                Price = productDto.Price,
+                ImageUrl = productDto.ImageUrl,
+                Id = new Guid()
+            };
+            _productRepository.AddProduct(product);
+            await _productRepository.SaveAllAsync();
+
+            return Ok(product);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAllProducts()
+        {
+            var products = await _productRepository.GetProducts();
+            return Ok(products);
         }
     }
 }
