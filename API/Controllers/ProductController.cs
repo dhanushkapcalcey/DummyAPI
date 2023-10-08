4696d1e4562fcb40999adafac37224f634ebb573
Dhanushka Premasiri <dhanushka.p@calcey.com>
2023-10-08 12:06:32 +0530

    Add ProductController

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public ProductController(DataContext dataContext) 
+        {
+            _dataContext = dataContext;
+        }
+    }
+}
