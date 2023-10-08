eeb04d9cdc8a29dfe723af5b97eeaeb1c42be160
Dhanushka Premasiri <dhanushka.p@calcey.com>
2023-10-08 15:43:24 +0530

    Create productDto

---
+++ API/DTOs/ProductDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.DTOs
+{
+    public class ProductDto
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int Quantity { get; set; }
+    }
+}
