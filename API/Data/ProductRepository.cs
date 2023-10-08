f1261dee730ee9ac4dcb6852133d09a4d81e8f24
Dhanushka Premasiri <dhanushka.p@calcey.com>
2023-10-08 14:53:34 +0530

    Create ProductRepository

---
+++ API/Data/ProductRepository.cs
@@ -0,0 +1,45 @@
+﻿using API.Entities;
+using API.Interfaces;
+
+namespace API.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly DataContext _context;
+
+        public ProductRepository(DataContext context) 
+        {
+            _context = context;
+        }
+
+        public void AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+        }
+
+        public void DeletableProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProductById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
