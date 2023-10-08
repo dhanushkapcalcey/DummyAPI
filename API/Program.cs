a35ce59b58d32e470f5ebb3a4c286df0baa6f5cf
Dhanushka Premasiri <dhanushka.p@calcey.com>
2023-10-08 21:24:59 +0530

    Configure AutoMapper

--- API/Program.cs
+++ API/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
