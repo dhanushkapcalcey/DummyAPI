677073c3edb8e71acbfffd2959f7870355b01281
Dhanushka Premasiri <dhanushka.p@calcey.com>
2023-10-08 14:15:08 +0530

    Add IProductRepository

---
+++ API/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+
+namespace API.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+        Product GetProductById(int id);
+        void AddProduct(Product product);
+        void DeletableProduct(int productId);
+        void UpdateProduct(Product product);
+        Task<bool> SaveAllAsync();
+
+    }
+}
